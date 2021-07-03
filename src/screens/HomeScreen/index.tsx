31a047c2322767055c950ad07d8a17a569ec9a28
eminkartci <emin.kartci@ozu.edu.tr>
2021-07-03 20:06:02 +0300

    Scroll View is used

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native'
 import ProductItem from '../../components/ProductItem';
 
 import { services } from '../../assets/data/services'
@@ -7,10 +7,16 @@ import { services } from '../../assets/data/services'
 const index = () => {
     return (
         <View>
-            <ProductItem service={services[0]} />
-            <ProductItem service={services[1]}/>
-            <ProductItem service={services[2]}/>
-            <ProductItem service={services[3]}/>
+            <ScrollView>
+                <ProductItem service={services[0]} />
+                <ProductItem service={services[1]}/>
+                <ProductItem service={services[2]}/>
+                <ProductItem service={services[3]}/>
+                <ProductItem service={services[0]} />
+                <ProductItem service={services[1]}/>
+                <ProductItem service={services[2]}/>
+                <ProductItem service={services[3]}/>
+            </ScrollView>
         </View> 
     );
 };
