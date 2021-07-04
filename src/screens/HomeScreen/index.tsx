c8deea2dbec0e814853886f0273ea70194980d66
eminkartci <emin.kartci@ozu.edu.tr>
2021-07-04 22:15:27 +0300

    images are new and local - new course

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, ScrollView } from 'react-native'
+import { View, Text, StyleSheet, Image, FlatList } from 'react-native'
 import ProductItem from '../../components/ProductItem';
 
 import { services } from '../../assets/data/services'
@@ -7,14 +7,10 @@ import { services } from '../../assets/data/services'
 const index = () => {
     return (
         <View>
-            <ScrollView>
-                <ProductItem service={services[0]} />
-                <ProductItem service={services[1]}/>
-                <ProductItem service={services[2]}/>
-                <ProductItem service={services[3]}/>
-                <ProductItem service={services[0]} />
-                <ProductItem service={services[1]}/>
-            </ScrollView>
+           <FlatList 
+            data={services}
+            renderItem={({item}) => <ProductItem service={item}/> }
+           />
         </View> 
     );
 };
